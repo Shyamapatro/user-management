d0965c3e568b8004753f3fc8e8dbe5d50cabbe97
Shyama patro <shyama.patro@gmail.com>
2024-08-14 09:28:38 +0530

    fix

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+
 const token = localStorage.getItem("token");
 if (token) {
   axios.defaults.headers.common["x-access-token"] = token; 
 }
-const API_URL = process.env.BACKEND_API_URL; 
+const API_URL = "https://user-backend-k20c.onrender.com/api/user"; 
 export const signup = (data) => axios.post(`${API_URL}/signup`, data);
 export const login = (data) => axios.post(`${API_URL}/login`, data);
 export const getUserDetails = () => axios.get(`${API_URL}/getUserDetails`);
